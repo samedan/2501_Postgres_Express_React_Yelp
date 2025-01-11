4ac38b95cf4d22bd8046c4c44948a83bbdf2dbee
Popescu Daniel <same.dan@gmail.com>
2025-01-11 12:02:21 +0100

    Context added

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router,  Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import UpdatePage from "./routes/UpdatePage";
 import RestaurantDetailPage from "./routes/RestaurantDetailPage";
+import { RestaurantsContextProvider } from "./context/RestaurantsContext";
 
 const App = () => {
   return (
     <div className="container">
-    <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/restaurants/:id/update" element={<UpdatePage />} />
-      <Route path="/restaurants/:id" element={<RestaurantDetailPage />} />
-    </Routes>
-  </Router>
-  </div>)
+      <RestaurantsContextProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/restaurants/:id/update" element={<UpdatePage />} />
+            <Route path="/restaurants/:id" element={<RestaurantDetailPage />} />
+          </Routes>
+        </Router>
+      </RestaurantsContextProvider>
+    </div>
+  );
 };
 
 export default App;
